e67a0b92660bc2f2ecab969d50611a48032b74cd
Stavan Dalal <dalalstavan7@gmail.com>
2026-04-25 21:13:00 +0530

    Connect frontend to backend using axios

--- src/CreateHabit.jsx
+++ src/CreateHabit.jsx
@@ -1,9 +1,35 @@
 import { useState, React} from 'react'
+import axios from 'axios';
 
 const CreateHabit = () => {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
     const [frequency, setFrequency] = useState('DAILY');
+
+    const handleSubmit = async () =>{
+        try{
+            const response = await axios.post(
+                'http://localhost:8080/api/habits',
+                {
+                    name,
+                    description,
+                    frequency
+                }
+            )
+            console.log("Response: ", response.data)
+            alert('Habit created successfully!');
+
+            //reset form
+            setName('');
+            setDescription('');
+            setFrequency('DAILY');
+            
+
+        }catch(error){
+            console.error("Error creating habit: ", error);
+            alert('Failed to create habit. Please try again.');
+        }
+    }
 
 
 
@@ -29,7 +55,7 @@ const CreateHabit = () => {
         <option value="MONTHLY">Monthly</option>
        </select>
 
-       <button>Create</button>
+       <button onClick={handleSubmit}>Create</button>
     </div>
   )
 }
